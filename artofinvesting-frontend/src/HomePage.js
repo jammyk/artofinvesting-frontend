ea228daaafe6c288e34745b84faa4dae38edbb1d
JH Kim <ehwo78@gmail.com>
2020-07-20 13:46:53 -0700

    Extracted searchbar into own module. Miscellaneous fixes

--- artofinvesting-frontend/src/HomePage.js
+++ artofinvesting-frontend/src/HomePage.js
@@ -1,75 +1,33 @@
 import React, { Component } from 'react';
-import { Input, Button, Row, Col, AutoComplete, Modal } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { Button, Row, Col, Modal } from 'antd';
 import logo from './aoi_logo.png';
 import LoginForm from './LoginForm.js';
+import CompanySearchBar from './CompanySearchBar';
 
 class HomePage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            searchValue: '',
-            options: [],
             isSignInPopUpVisible: false
         };
 
-        this.onSearch = this.onSearch.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.setOptions = this.setOptions.bind(this);
-        this.setSearchValue = this.setSearchValue.bind(this);
-        this.onSignInButtonClick = this.onSignInButtonClick.bind(this);
-        this.handleOk = this.handleOk.bind(this);
-        this.handleCancel = this.handleCancel.bind(this);
     }
 
-    mockVal(str, repeat=1) {
-        return { value: str.repeat(repeat) };
-    }
-
-    onSelect(data) {
-        console.log('onSelect', data);
-    }
-
-    onSearch(data) {
-        this.setOptions(
-            !data ? [] : [this.mockVal(data), this.mockVal(data, 2), this.mockVal(data, 3)]
-        )
-        console.log("Crashed not!");
-    }
-
-    onChange(data) {
-        this.setSearchValue(data);
-    }
-
-    setOptions(newOptions) {
-        console.log(newOptions);
-        console.log("CRASHED!")
-        this.setState((state, props) => ({
-            options: newOptions
-        }));
-    }
-
-    setSearchValue(value) {
-        this.setState((state, props) => ({
-            searchValue: value
-        }));
-    }
-
-    onSignInButtonClick() {
+    onSignInButtonClick = () => {
         this.setState({
             isSignInPopUpVisible: true
         });
     }
 
-    handleOk(e) {
+    handleOk = (e) => {
         console.log(e);
         this.setState({
             isSignInPopUpVisible: false,
         });
     }
 
-    handleCancel(e) {
+    handleCancel = (e) => {
         console.log(e);
         this.setState({
             isSignInPopUpVisible: false,
@@ -97,16 +55,7 @@ class HomePage extends Component {
                 </Row>
                 <Row justify="center" align="top" style={{ "paddingTop": "20px" }}>
                     <Col span={10}>
-                        <AutoComplete 
-                            options={this.state.options} 
-                            onSelect={this.onSelect} 
-                            onSearch={this.onSearch}
-                            onChange={this.onChange}
-                            style={{
-                                width: "100%"
-                            }}>
-                        <Input addonBefore={<SearchOutlined />} placeholder="Search for companies using their ticker " />
-                        </AutoComplete>
+                        <CompanySearchBar />
                     </Col>
                 </Row>
                 <div style={{ "paddingTop": "20px" }}>
