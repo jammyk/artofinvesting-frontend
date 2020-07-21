1681c4b2dad0aec7ed7d337317de048a8311e45f
Thomas Kwun <taekheekwun@gmail.com>
2020-07-21 14:00:15 -0700

    Screener Page: add link to logo, fix css for hover

--- artofinvesting-frontend/src/components/Screener.js
+++ artofinvesting-frontend/src/components/Screener.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Steps, Button, message, Popover, Row, Col, Input, Radio } from 'antd'
+import { Link } from 'react-router-dom';
 import '../stylesheets/Screener.css'
 import magicFormula from '../images/magicFormula.png'
 import spinoff from '../images/spinoff.png'
@@ -47,12 +48,12 @@ class Screener extends Component {
     selectScreener = s => {
         const selected = s.currentTarget.value
         this.toggleHighlight(selected)
-         if(this.state.selected !== selected){
+        if (this.state.selected !== selected) {
             this.state.selected = selected
-         } else {this.state.selected = "none"}
-         this.toggleNextButton()
-         this.toggleDoneButton()
-         console.log(this.state.selected)
+        } else { this.state.selected = "none" }
+        this.toggleNextButton()
+        this.toggleDoneButton()
+        console.log(this.state.selected)
     }
 
     selectNumStocks = n => {
@@ -69,35 +70,35 @@ class Screener extends Component {
     }
 
     onClickDone() {
-        if(this.state.current === 1 ){
+        if (this.state.current === 1) {
             var marketCap = this.state.marketCap
-            if(!marketCap.match(/^[0-9]+([,.][0-9]+)?$/)){
+            if (!marketCap.match(/^[0-9]+([,.][0-9]+)?$/)) {
                 message.error("Please input correct Minimum Market Cap")
             }
-            else if(typeof this.state.marketCap === 'string'){
+            else if (typeof this.state.marketCap === 'string') {
                 this.state.marketCap = parseFloat(this.state.marketCap)
                 message.success('Processing complete!')
             }
-            
+
             console.log(this.state)
         }
     }
 
     toggleDoneButton() {
-        if(this.state.selected === "none" || this.state.selected === "magic_formula")
+        if (this.state.selected === "none" || this.state.selected === "magic_formula")
             document.getElementById("doneButton").style.visibility = "hidden"
         else document.getElementById("doneButton").style.visibility = "visible"
     }
 
     toggleNextButton() {
-        if(this.state.selected === "magic_formula")
+        if (this.state.selected === "magic_formula")
             document.getElementById("nextButton").style.visibility = "visible"
         else document.getElementById("nextButton").style.visibility = "hidden"
     }
 
     toggleHighlight(selected) {
-        if(this.state.selected !== selected){
-            if(this.state.selected !== "none")
+        if (this.state.selected !== selected) {
+            if (this.state.selected !== "none")
                 document.getElementById(this.state.selected).style.removeProperty("border")
             document.getElementById(selected).style.border = "#23CDF9 thin dotted"
         }
@@ -110,7 +111,7 @@ class Screener extends Component {
         return (
             <div>
                 <Row>
-                    <img src={logo} alt="logo" style={{width: "100px", height: "100px", paddingTop: "1%", paddingLeft: "1%"}}/>
+                    <Link to="/"><img src={logo} alt="logo" style={{ width: "100px", height: "100px", paddingTop: "1%", paddingLeft: "1%" }} /></Link>
                 </Row>
                 <Row style={{ paddingTop: "10%" }} justify="space-around" align="middle">
                     <Col span={4} justify="middle" className="steps-label">
@@ -122,15 +123,15 @@ class Screener extends Component {
                     <Col span={16} justify="top" className="steps-content">
                         {current === 0 && (
                             <div>
-                                <Button type='text' value="spinoff" style={{ paddingRight: "10%", paddingBottom: "10%"}} onClick={this.selectScreener}>
-                                    <img src={spinoff} id="spinoff" alt="Spinoff" style={{ width: "200px", height: "200px"}}/>
+                                <Button className="screener-btn" type='text' value="spinoff" style={{ paddingRight: "10%", paddingBottom: "10%" }} onClick={this.selectScreener}>
+                                    <img src={spinoff} id="spinoff" alt="Spinoff" style={{ width: "200px", height: "200px" }} />
                                     <br />Spinoff
                                 </Button>
-                                <Button type='text' value="magic_formula" style={{ paddingRight: "10%", paddingBottom: "10%" }} onClick={this.selectScreener}>
+                                <Button className="screener-btn" type='text' value="magic_formula" style={{ paddingRight: "10%", paddingBottom: "10%" }} onClick={this.selectScreener}>
                                     <img src={magicFormula} id="magic_formula" alt="Magic Formula" style={{ width: "200px", height: "200px" }} />
                                     <br />Magic Formula
                                 </Button>
-                                <Button type='text' value="share_cannibals" style={{ paddingRight: "10%", paddingBottom: "10%" }} onClick={this.selectScreener}>
+                                <Button className="screener-btn" type='text' value="share_cannibals" style={{ paddingRight: "10%", paddingBottom: "10%" }} onClick={this.selectScreener}>
                                     <img src={shareCannibal} id="share_cannibals" alt="Share Cannibals" style={{ width: "200px", height: "200px" }} />
                                     <br />Share Cannibals
                                 </Button>
@@ -139,11 +140,11 @@ class Screener extends Component {
                         {current === 1 && (
                             <div>
                                 <Row align="center" >
-                                    <Col span={4} style={{paddingBottom: "5%", paddingTop: "5%"}}>
+                                    <Col span={4} style={{ paddingBottom: "5%", paddingTop: "5%" }}>
                                         <h4>Minimum Market Cap</h4>
                                     </Col>
-                                    <Col span={6} style={{paddingBottom: "5%", paddingTop: "5%"}}>
-                                        <Input placeholder="Enter Market Cap" justify="top" addonAfter="Million" onChange={this.inputMinMarketCap} style={{textAlign: "right"}}/>
+                                    <Col span={6} style={{ paddingBottom: "5%", paddingTop: "5%" }}>
+                                        <Input placeholder="Enter Market Cap" justify="top" addonAfter="Million" onChange={this.inputMinMarketCap} style={{ textAlign: "right" }} />
                                     </Col>
                                 </Row>
                                 <Row align="center">
@@ -165,16 +166,16 @@ class Screener extends Component {
                     <Col span={14} justify="top" align="end">
                         <div style={{ paddingTop: "10%" }}>
                             {current > 0 && (
-                                <Button style={{ margin: '0 8px'}} type="primary" onClick={() => this.prev()}>
+                                <Button style={{ margin: '0 8px' }} type="primary" onClick={() => this.prev()}>
                                     Previous
                                 </Button>
                             )}
                             {current < this.state.numSteps - 1 && (
-                                <Button id="nextButton" style={{ margin: '0 8px', visibility:"hidden"}} type="primary" onClick={() => this.next()}>
+                                <Button id="nextButton" style={{ margin: '0 8px', visibility: "hidden" }} type="primary" onClick={() => this.next()}>
                                     Next
                                 </Button>
                             )}
-                            <Button id="doneButton" style={{ margin: '0 8px',  visibility:"hidden" }} type="primary" onClick={() => this.onClickDone()}>
+                            <Button id="doneButton" style={{ margin: '0 8px', visibility: "hidden" }} type="primary" onClick={() => this.onClickDone()}>
                                 Done
                             </Button>
 
