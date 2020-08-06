c05020834b0d41e5e1e34a1b2e32cea867b6d06e
Joshua <blueseakr24@gmail.com>
2020-08-06 10:09:28 -0700

    some design

--- artofinvesting-frontend/src/components/ArtistQuarterChanges.js
+++ artofinvesting-frontend/src/components/ArtistQuarterChanges.js
@@ -19,11 +19,15 @@ class ArtistQuarterChanges extends Component {
             <div className="site-card-wrapper">
                 <Row gutter={16}>
                     <Col span={8}>
-                        <Card title="Buys" bordered={true}>
+                        <Card title="Buys" bordered={true} headStyle={{backgroundColor: "#cce6ff"}} bodyStyle={{paddingTop: "1%"}}>
+                        <div style={{clear: "both"}}>
+                            <p className="name">Name</p>
+                            <p className="percentage">% Change</p>
+                        </div>
                         {buys.map((buys, index) => {
                             return <div style={{clear: "both"}}>
-                                    <p className="name"><strong>{buys.ticker}</strong>&nbsp;{buys.name}</p>
-                                    <p className="percentage">{buys.percentChange}</p>
+                                    <p className="name"><a href="/details"><strong>{buys.ticker}</strong>&nbsp;{buys.name}</a></p>
+                                    <p className="percentage" id="b">{buys.percentChange}</p>
                                 </div>
                                 
                             
@@ -31,11 +35,15 @@ class ArtistQuarterChanges extends Component {
                         </Card>
                     </Col>
                     <Col span={8}>
-                        <Card title="Sells" bordered={true}>
+                        <Card title="Sells" bordered={true} headStyle={{backgroundColor: "#cce6ff"}} bodyStyle={{paddingTop: "1%"}}>
+                        <div style={{clear: "both"}}>
+                            <p className="name">Name</p>
+                            <p className="percentage">% Change</p>
+                        </div>
                         {sells.map((sells, index) => {
                             return <div style={{clear: "both"}}>
-                                    <p className="name"><strong>{sells.ticker}</strong>&nbsp;{sells.name}</p>
-                                    <p className="percentage">{sells.percentChange}</p>
+                                    <p className="name"><a href="/details"><strong>{sells.ticker}</strong>&nbsp;{sells.name}</a></p>
+                                    <p className="percentage" id="s">{sells.percentChange}</p>
                                 </div>
                                 
                             
